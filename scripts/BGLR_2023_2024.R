278d85406cb4ed696da0dd3f1c178c4b56b0b5a7
pthyde123 <pth7@cornell.edu>
2024-12-10 12:39:14 -0500

    start of BGLR

---
+++ scripts/BGLR_2023_2024.R
@@ -0,0 +1,20 @@
+
+
+
+
+yTraits <- as.matrix(dplyr::select(grainWgt, contains("Yield")))
+incLocations <- model.matrix(~ -1 + locationName, grainWgt)
+incBlocks <- model.matrix(~ -1 + blockNumberF, grainWgt)
+incOatAcc <- model.matrix(~ -1 + germplasmName, grainWgt)
+incPeaAcc <- model.matrix(~ -1 + peaAcc, grainWgt)
+
+
+ETA <- list(list(X=incLocations, model="FIXED"),
+            list(X=incBlocks, model="BRR"),
+            list(X=incOatAcc, model="BRR"),
+            list(X=incPeaAcc, model="BRR"))
+Specifications
+
+
+
+
